ef513ddf1422793559466996114d7f0852858f80
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-16 22:06:29 -0700

    added short and long url to website and implemented function correctly

--- express_server.js
+++ express_server.js
@@ -33,24 +33,28 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:shortsURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: req.params.longURL,
+    longURL: urlDatabase[req.params.shortURL],
   };
+
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const random = Math.random().toString(36).slice(2, 8);
+
+  urlDatabase[random] = req.body.longURL;
+
+  res.redirect(`/urls/${random}`);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-function generateRandomString() {
-  let random = Math.random().toString(36).slice(1, 7);
-  return random;
-}
