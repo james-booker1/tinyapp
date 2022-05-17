6ee8420bbbeb492642aa0853a0f53acf4df44705
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-16 18:41:48 -0700

    Added shortURL route parameter

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("viewengine", "ejs");
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -21,11 +21,19 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortsURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
