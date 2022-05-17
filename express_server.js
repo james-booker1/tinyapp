6be1187c4cf1d2076e5f50d149d9e20a98e2eff5
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-16 20:32:53 -0700

    Added random string generator to provide a short url

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -26,6 +29,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortsURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -34,6 +41,16 @@ app.get("/urls/:shortsURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let random = Math.random().toString(36).slice(1, 7);
+  return random;
+}
