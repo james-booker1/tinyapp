f08e6c0e7eccb30b72e3cec6d5875a2d8441600c
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-16 18:15:23 -0700

    Adding basic html outline

--- express_server.js
+++ express_server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("viewengine", "ejs")
+app.set("viewengine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -23,4 +23,9 @@ app.get("/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
