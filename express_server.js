1fbc19e43132571a30c0afab825b8bb894b10923
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-05-18 12:58:51 -0700

    working registration page

--- express_server.js
+++ express_server.js
@@ -12,31 +12,32 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-// Login
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  if (!username) {
-    return res.status(401).send("Wrong password - Try again");
-  }
-  res.cookie("username", username);
-  res.redirect("/urls");
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls", (req, res) => {
+  console.log("users", users);
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
+});
+
+//Generates random short URL
+app.post("/urls", (req, res) => {
+  const shortURL = Math.random().toString(36).slice(2, 8);
+
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -50,11 +51,16 @@ app.get("/urls/:shortURL", (req, res) => {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
-  res.render("urls_new", templateVars);
-  res.render("urls_index", templateVars);
+
   res.render("urls_show", templateVars);
 });
+//edits the index page and redirects to the long URL changer on show
+// app.get("/urls/:shortURL", (req, res) => {
+//   const shortURL = req.params.shortURL;
 
+//   res.redirect(`/urls/${shortURL}`);
+//   //res.redirect("urls_show");
+// });
 //LongURL updater, keeps the shortened URL but changes the long to a new website.
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -68,29 +74,56 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
-//edits the index page and redirects to the long URL changer on show
-app.post("/urls/:shortURL/edit", (req, res) => {
-  const shortURL = req.params.shortURL;
-
-  res.redirect(`/urls/${shortURL}`);
-});
 // delete button in the index page
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
-app.post("/urls", (req, res) => {
-  const random = Math.random().toString(36).slice(2, 8);
-
-  urlDatabase[random] = req.body.longURL;
-
-  res.redirect(`/urls/${random}`);
-});
-
+//Logout
 app.post("/logout", (req, res) => {
   console.log("logout");
   res.clearCookie("username");
+  res.redirect("/register");
+});
+// Login
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  if (!username) {
+    return res.status(401).send("Wrong password - Try again");
+  }
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+app.get("/register", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  const randomId = Math.random().toString(36).slice(2, 8);
+  users[req.body.id] = {
+    id: randomId,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  res.cookie("user_id", randomId);
   res.redirect("/urls");
 });
 
